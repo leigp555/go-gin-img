275237e1e5f9b34ad70d419018a3e6befc215c9b
leigp555 <907090585@qq.com>
2023-03-18 01:48:18 +0800

    feat: 添加token解析的中间件

---
+++ middleware/token_verify.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"img/server/global"
+	"img/server/models"
+	"img/server/utils"
+	"strings"
+)
+
+func TokenVerify() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var mdb = global.Mydb
+		//从请求头获取token
+		tokenHeader := c.GetHeader("Authorization")
+
+		//从请求头获取token失败
+		if tokenHeader == "" {
+			c.JSON(401, gin.H{"msg": "请上传身份凭证"})
+			c.Abort()
+			return
+		}
+		//拆分出token
+		splitArr := strings.Split(tokenHeader, " ")
+		tokenStr := splitArr[1]
+		//解析token 解析失败阻止后续中间件执行
+		userId, err2 := utils.ParseToken(tokenStr)
+		if err2 != nil {
+			c.JSON(403, gin.H{"msg": "用户身份过期,请重新登录"})
+			c.Abort()
+			return
+		}
+		var dbUser = models.User{}
+		mdb.Where("id = ?", userId).First(&dbUser)
+		//将用户的信息传给其余的中间件
+		c.Set("userId", dbUser.ID)
+		c.Set("userEmail", dbUser.Email)
+	}
+}
