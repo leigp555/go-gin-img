446e93f4f5e02f0fb67b21991dbe763c15c532b0
leigp555 <907090585@qq.com>
2023-03-17 16:03:26 +0800

    fix: 修复yaml文件错误导致读取配置失败的问题

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"img/server/core"
-	"img/server/global"
 )
 
 func main() {
 	core.InitConf()
-	fmt.Println(global.Config)
-	fmt.Println("end ... ")
+	core.LinkMysqlDB()
+	core.LinkRedisDB()
 }
