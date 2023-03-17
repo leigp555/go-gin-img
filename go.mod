f406006cbbba0973df61979dbe1f0607a49f67da
leigp555 <907090585@qq.com>
2023-03-18 01:51:45 +0800

    feat: 添加图形验证码的生成与解析的方法

--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -33,6 +34,7 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mojocn/base64Captcha v1.3.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
@@ -40,6 +42,7 @@ require (
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/image v0.0.0-20190501045829-6d32002ffd75 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
---
+++ utils/captcha_deal.go
@@ -0,0 +1,70 @@
+package utils
+
+import (
+	"github.com/mojocn/base64Captcha"
+)
+
+// configJsonBody json request body.
+type configJsonBody struct {
+	Id            string
+	CaptchaType   string
+	VerifyValue   string
+	DriverAudio   *base64Captcha.DriverAudio
+	DriverString  *base64Captcha.DriverString
+	DriverChinese *base64Captcha.DriverChinese
+	DriverMath    *base64Captcha.DriverMath
+	DriverDigit   *base64Captcha.DriverDigit
+}
+
+var store = base64Captcha.DefaultMemStore
+
+// GetCaptcha 生成验证码
+func GetCaptcha() (string, string, error) {
+	var param = configJsonBody{
+		Id:          "",
+		CaptchaType: "",
+		VerifyValue: "",
+		DriverAudio: &base64Captcha.DriverAudio{},
+		DriverString: &base64Captcha.DriverString{
+			Length:          4,
+			Height:          40,
+			Width:           100,
+			ShowLineOptions: 2,
+			NoiseCount:      0,
+			Source:          "1234567890",
+		},
+		DriverChinese: &base64Captcha.DriverChinese{},
+		DriverMath: &base64Captcha.DriverMath{
+			Height: 40,
+			Width:  100,
+		},
+		DriverDigit: &base64Captcha.DriverDigit{
+			Height:   40,
+			Width:    100,
+			Length:   4,
+			MaxSkew:  0.4,
+			DotCount: 50,
+		},
+	}
+	var driver base64Captcha.Driver
+	//create base64 encoding captcha
+	switch param.CaptchaType {
+	case "audio":
+		driver = param.DriverAudio
+	case "string":
+		driver = param.DriverString.ConvertFonts()
+	case "math":
+		driver = param.DriverMath.ConvertFonts()
+	case "chinese":
+		driver = param.DriverChinese.ConvertFonts()
+	default:
+		driver = param.DriverDigit
+	}
+	c := base64Captcha.NewCaptcha(driver, store)
+	return c.Generate()
+}
+
+// VerifyCaptcha 解析验证码
+func VerifyCaptcha(id, VerifyValue string) bool {
+	return store.Verify(id, VerifyValue, true)
+}
