ae189eb85d1ed5c08f2195cd51ff3e786ca256e5
leigp555 <907090585@qq.com>
2023-03-26 02:14:38 +0800

    feat: 规范图形验证码生成接口的客户端返回内容

--- api/public_api/send_img_captcha.go
+++ api/public_api/send_img_captcha.go
@@ -8,7 +8,8 @@ import (
 func (PublicApi) SendImgCaptcha(c *gin.Context) {
 	id, captcha, err := utils.Captcha.Generate()
 	if err != nil {
-		c.JSON(500, gin.H{"code": 500, "errors": map[string]any{"body": []string{"服务器异常，请重试"}}})
+		utils.DealErr(c, err, "图形验证码生成失败")
+		return
 	}
 	c.JSON(200, gin.H{"code": 200, "msg": "验证码获取成功", "data": map[string]any{"captchaId": id, "captchaImg": captcha}})
 }
