f947e4924bfd1ee19dc5f24aa7e99194a4d08d48
leigp555 <907090585@qq.com>
2023-03-25 16:51:15 +0800

    feat: mysql日志配置

--- core/mysql_init.go
+++ core/mysql_init.go
@@ -6,16 +6,27 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"img/server/global"
+	"log"
+	"os"
 	"time"
 )
 
 // LinkMysqlDB LinkDB 连接mysql数据库
 func LinkMysqlDB() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             time.Second,   // 慢 SQL 阈值
+			LogLevel:                  logger.Silent, // 日志级别
+			IgnoreRecordNotFoundError: true,          // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  false,         // 禁用彩色打印
+		},
+	)
 	// 获取mysql的配置
 	var mysqlConf = global.Config.Mysql
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True", mysqlConf.Username, mysqlConf.Password, mysqlConf.Addr, mysqlConf.DB)
 	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info), //配置一个日志
+		Logger: newLogger, //配置一个日志
 	})
 	if err != nil {
 		global.SugarLog.Fatalf("mysql数据库连接失败%v\n", err)
