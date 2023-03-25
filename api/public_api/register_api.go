5fb35609a8ce2b5d46957b0ba650d92faf7f8c03
leigp555 <907090585@qq.com>
2023-03-26 02:07:26 +0800

    feat: 加强了对用户信息的验证

--- api/public_api/register_api.go
+++ api/public_api/register_api.go
@@ -12,13 +12,13 @@ import (
 // Register 处理用户注册
 func (PublicApi) Register(c *gin.Context) {
 	type NewUserInfo struct {
-		Username         string `form:"username" binding:"required,min=1,max=20" msg:"用户名不能为空,且长度为1~20位"`
+		Username         string `form:"username" binding:"required,alphanum,min=1,max=20" msg:"用户名不能为空,且长度为1~20位数字或字母"`
 		Email            string `form:"email" binding:"required,email" msg:"请输入正确的邮箱"`
 		EmailCaptchaCode string `form:"emailCaptcha" binding:"required,len=6" msg:"邮箱验证码不正确"`
-		Password         string `form:"password" binding:"required,min=6,max=12" msg:"密码不能为空,且长度为6~12位"`
+		Password         string `form:"password" binding:"required,min=6,max=12,alphanum" msg:"密码不能为空,且长度为6~12位数字或字母"`
 		RePassword       string `form:"rePassword" binding:"required,min=6,max=12,eqfield=Password" msg:"两次输入的密码不一致"`
 		ImgCaptchaId     string `form:"imgCaptchaId" binding:"required" msg:"图形验证码ID不能为空"`
-		ImgCaptcha       string `form:"imgCaptcha" binding:"required" msg:"图形验证码不正确"`
+		ImgCaptcha       string `form:"imgCaptcha" binding:"required,numeric" msg:"图形验证码不正确"`
 	}
 
 	var newUserInfo NewUserInfo
