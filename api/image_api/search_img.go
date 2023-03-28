5d60c105aef161014f6c9ab942fbfcbaed6acb14
leigp555 <907090585@qq.com>
2023-03-28 20:10:56 +0800

    feat: 将public下的文件托管到static路径上

--- api/image_api/search_img.go
+++ api/image_api/search_img.go
@@ -1,16 +1,17 @@
 package image_api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
 func (ImgApi) SearchImg(c *gin.Context) {
 	fileName := c.Query("fileName")
-	file, err := os.ReadFile("./public/" + fileName + ".jpg")
-	if err != nil {
-		c.JSON(500, gin.H{"msg": "读取文件失败"})
-		return
-	}
-	c.JSON(200, gin.H{"msg": "success", "data": file})
+	//file, err := os.ReadFile("./public/" + fileName + ".jpg")
+	//if err != nil {
+	//	c.JSON(500, gin.H{"msg": "读取文件失败"})
+	//	return
+	//}
+	imgUrl := fmt.Sprintf("http://localhost:8080/static/%s", fileName)
+	c.JSON(200, gin.H{"msg": "success", "data": imgUrl})
 }
