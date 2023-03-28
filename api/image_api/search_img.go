38f902dd9aaa0f6a7da3b5d5a92aa1a1f3055047
leigp555 <907090585@qq.com>
2023-03-28 19:59:23 +0800

    feat: 添加获取图片base64文件的处理函数

--- api/image_api/search_img.go
+++ api/image_api/search_img.go
@@ -1,1 +1,16 @@
 package image_api
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func (ImgApi) SearchImg(c *gin.Context) {
+	fileName := c.Query("fileName")
+	file, err := os.ReadFile("./public/" + fileName + ".jpg")
+	if err != nil {
+		c.JSON(500, gin.H{"msg": "读取文件失败"})
+		return
+	}
+	c.JSON(200, gin.H{"msg": "success", "data": file})
+}
