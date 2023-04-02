dbb60080ebd15d759fa7edd1cb5f62230c32cd52
leigp555 <907090585@qq.com>
2023-04-02 23:53:44 +0800

    feat: 优化缩略图生成的方式

--- api/image_api/upload_img.go
+++ api/image_api/upload_img.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"img/server/utils"
 	"log"
+	"mime/multipart"
 	"strings"
 	"time"
 )
@@ -27,10 +28,13 @@ func (ImgApi) UploadImg(c *gin.Context) {
 		c.JSON(400, gin.H{"msg": "只接收扩展名为 image/png, image/jpeg, image/gif, image/jpg的图片"})
 		return
 	}
-	//生成图片名
+	//生成图片名和图片ID以及存储路径
 	username := c.GetString("userId")
-	fileName := utils.Md5Str(fmt.Sprintf("%s-%s-%s", time.Now().Format("2006/01/02"), username, file.Filename)) + "." + nameArr[1]
+	fileId := utils.Md5Str(fmt.Sprintf("%s-%s-%s", time.Now().Format("2006/01/02"), username, file.Filename))
+	fileName := fileId + "." + nameArr[1]
 	dst := "./public/img/" + fileName
+	//将图片路径以及id添加到数据库中
+
 	// 上传文件至指定的完整文件路径
 	err = c.SaveUploadedFile(file, dst)
 	if err != nil {
@@ -39,22 +43,34 @@ func (ImgApi) UploadImg(c *gin.Context) {
 	}
 	c.JSON(200, gin.H{"file": fileName, "size": file.Size})
 
+	//响应结束生成图片缩略图
 	c.Next()
-
-	//生成图片缩略图
-	//获取源文件
-	filePath := fmt.Sprintf("./public/img/%s", fileName)
-	src, err := imaging.Open(filePath)
+	// 打开上传的文件
+	src, err := file.Open()
 	if err != nil {
 		log.Fatalf("failed to open image: %v", err)
+		return
+	}
+	defer func(src multipart.File) {
+		err := src.Close()
+		if err != nil {
+			log.Fatalf("failed to close image: %v", err)
+		}
+	}(src)
+
+	// 解码上传的图像
+	img, err := imaging.Decode(src)
+	if err != nil {
+		log.Fatalf("failed to open image: %v", err)
+		return
 	}
 	//调整缩略图参数
-	src = imaging.Resize(src, 400, 0, imaging.Lanczos)
-	img := imaging.Blur(src, 2)
+	thumb := imaging.Thumbnail(img, 400, 0, imaging.Lanczos)
+	result := imaging.Blur(thumb, 2)
 	//生成缩略图存储路径
 	thumbFilePath := fmt.Sprintf("./public/thumb/%s%s", "thumb", fileName)
 	// 保存缩略图到文件
-	err = imaging.Save(img, thumbFilePath)
+	err = imaging.Save(result, thumbFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
